d97e30f92ce7a98ffceb32a5b907879639cfe5ba
unknown <filipebarbosa94@hotmail.com>
2020-06-09 16:15:34 -0300

    primeiro commit do problema numeros aleatorios

---
+++ problema1/guessing_game.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync');//importa o readline-sync para ler algo digitado
+
+//Gera numero aleatório
+const aleatorio = parseInt(Math.random()* 10)+1;
+
+const dica = (entrada)=>{
+    if (entrada > aleatorio){
+        return " O numero digitado é maior!";
+
+    }
+    if (entrada < aleatorio){
+        return "O numedo digitado é menor";
+
+    }
+}
+
+console.log("Bem Vindo ao jogo de Advinhação!!");
+
+while (true){
+    const gerado = readline.question("Digite um número");
+    if (gerado ==aleatorio){
+        console.log("Parabens você advinhou o numero");
+        break;
+    }
+    console.log(dica(gerado));
+}
