eeab823154dbb727a012db50860172ac8759296c
unknown <filipebarbosa94@hotmail.com>
2020-06-09 16:20:13 -0300

    Alteração de teste com nova branch

--- problema1/guessing_game.js
+++ problema1/guessing_game.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync');//importa o readline-sync para ler algo digitado
-
+//Alteração de teste
 //Gera numero aleatório
 const aleatorio = parseInt(Math.random()* 10)+1;
 
@@ -9,7 +9,7 @@ const dica = (entrada)=>{
 
     }
     if (entrada < aleatorio){
-        return "O numedo digitado é menor";
+        return "O numero digitado é menor";
 
     }
 }
