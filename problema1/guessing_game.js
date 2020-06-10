e6df4904cd843f53f09c25f071d9920f943fd28f
unknown <filipebarbosa94@hotmail.com>
2020-06-10 13:56:34 -0300

    inclusã do contador de tentativas

--- problema1/guessing_game.js
+++ problema1/guessing_game.js
@@ -3,6 +3,15 @@ const readline = require('readline-sync');//importa o readline-sync para ler alg
 //Gera numero aleatório
 const aleatorio = parseInt(Math.random()* 10)+1;
 
+
+function verificacontador(){
+
+    if (count == 5){
+        console.log("Suas tentativas acabaram");
+    }
+    
+};
+
 const dica = (entrada)=>{
     if (entrada > aleatorio){
         return " O numero digitado é maior!";
@@ -12,15 +21,22 @@ const dica = (entrada)=>{
         return "O numero digitado é menor";
 
     }
+   
 }
 
 console.log("Bem Vindo ao jogo de Advinhação!!");
+console.log("você terá 5 tentativas para advinhar o numero, Boa sorte!!");
+let count = 0;
 
-while (true){
+while (count<5){
     const gerado = readline.question("Digite um número");
     if (gerado ==aleatorio){
         console.log("Parabens você advinhou o numero");
         break;
     }
-    console.log(dica(gerado));
+    count++;
+ console.log(dica(gerado));
+
 }
+
+verificacontador();
